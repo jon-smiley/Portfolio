ba7ce9cd24f5c7a26de1a64964e9aa56e86f945d
Jon Smiley <jasmiley0715@jons-mbp.attlocal.net>
2017-12-29 14:02:07 -0600

    carousel almost functional

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,44 @@
 
+
+$("#link_home").on("click", function(){
+	console.log("home");
+});
+
+$("#link_resume").on("click", function(){
+	console.log("resume");
+});
+
+$("#link_bio").on("click", function(){
+	console.log("bio");
+});
+
+// ------------top nav bar---------------------------
+$(document).ready(function(){
+
+$('#myCarousel').carousel("pause");
+
+// Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+    
+
+
+// Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+
+});
+
+
+
